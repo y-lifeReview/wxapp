a43700fef2c80f2305220200de71196d681518d3
yangchao508 <1847868423@qq.com>
2020-06-20 22:55:39 +0800

    添加加载中动画

--- request/index.js
+++ request/index.js
@@ -1,4 +1,11 @@
+let ajaxTimes=0;//发送异步代码的次数
 export const  request=(params)=>{
+    ajaxTimes++;
+    //设置加载中样式
+    wx.showLoading({
+        title: "加载中",
+        mask: true,
+    });
     const baseUrl="https://api-hmugo-web.itheima.net/api/public/v1"
 
     return new Promise((resolve,reject)=>{
@@ -11,6 +18,13 @@ export const  request=(params)=>{
             },
             fail:(err)=>{
                 reject(err);
+            },
+            complete:()=>{
+                ajaxTimes--;
+                if(ajaxTimes===0){
+                    wx.hideLoading();//关闭加载中样式
+                }
+                
             }
         })
     })
