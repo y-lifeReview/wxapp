9a5e9547ecb903479c97189b64653fe9892c8fef
yangchao508 <1847868423@qq.com>
2020-06-21 00:20:49 +0800

    商品详情页面

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,20 +1,48 @@
 // pages/goods_detail/index.js
+import {
+  request
+} from "../../request/index";
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj:{}
   },
-
+  //商品对象
+  GoodsInfo:{},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {goods_id}=options
+    this.getGoodsDetail(goods_id)
   },
-
+  //获取商品详情数据
+  async getGoodsDetail(goods_id){
+    const goodsObj=await request({url:"/goods/detail",data:{goods_id}})
+    this.GoodsInfo=goodsObj;
+    this.setData({
+      goodsObj:{
+        goods_name:goodsObj.goods_name,
+        goods_price:goodsObj.goods_price,
+        //部分iphone不支持.webp图片 替换成.jpg 确保后台有.jpg图片
+        goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg'),
+        pics:goodsObj.pics
+      }
+    })
+  },
+  handelPreviewImage(e){
+    //先构造要预览的图片数组
+      const urls=this.GoodsInfo.pics.map(v=>v.pics_mid)
+      const current=e.currentTarget.dataset.url
+    wx.previewImage({
+      current:current,
+      urls: urls
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
